c06d677acabc5278baf3d35ae5312b1b69cf2636
Asyurazhr <Asyuradjola2004@gmail.com>
2026-01-02 23:29:01 +0700

    Add Firestore student repository

---
+++ app/src/main/java/com/example/firebase/repositori/RepositorySiswa.kt
@@ -0,0 +1,13 @@
+package com.example.firebase.repositori
+
+import com.example.firebase.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+interface RepositorySiswa {
+    suspend fun getDataSiswa(): List<Siswa>
+    suspend fun postDataSiswa(siswa: Siswa)
+}
+class FirebaseRepositorySiswa: RepositorySiswa{
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("siswa")
