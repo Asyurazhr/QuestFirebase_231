4ddaa0fee34b7fa46aba3b181e83b86432fccd35
Asyurazhr <Asyuradjola2004@gmail.com>
2026-01-08 19:53:49 +0700

    Update EditSiswaScreen params

--- app/src/main/java/com/example/firebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/firebase/view/HalamanEdit.kt
@@ -16,3 +16,8 @@ import kotlinx.coroutines.launch
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
